7fca01788cddb45873fec78528fe29cbed020e82
jorenouard <146163243+jorenouard@users.noreply.github.com>
2024-05-16 15:20:52 +0200

    en cours

---
+++ ex01/phoneBook.hpp
@@ -0,0 +1,14 @@
+#ifndef __PHONEBOOK__
+    #define __PHONEBOOK__
+    #include <string>
+    #include "contact.hpp"
+
+    class PhoneBook
+    {
+        public:
+            PhoneBook();
+
+        private:
+    };
+
+#endif
