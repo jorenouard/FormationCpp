c9e4fdb5e6b9707ecd1b8422cb3e45b9bfbd44fd
jorenouard <146163243+jorenouard@users.noreply.github.com>
2024-05-21 17:26:13 +0200

    doublons

--- ex12 lambda/main.cpp
+++ ex12 lambda/main.cpp
@@ -19,9 +19,16 @@ void printReverse(vector<int> v)
     cout << endl;
 }
 
-void bigToSmall(vector<int> v)
+void bigToSmall(vector<int>& v)
 {
     sort(v.begin(), v.end(), [](int a, int b) {return a > b;});
+    printVector(v);
+}
+
+void supprConsecutiveDoublons(vector<int> v)
+{
+    auto last = std::unique(v.begin(), v.end());
+    v.erase(last, v.end());
     printVector(v);
 }
 
@@ -29,10 +36,11 @@ void bigToSmall(vector<int> v)
 int main(){
     vector<int> v {4, 1, 3, 5, 2, 3, 1, 7};
 
-    cout << "number of values in v above 5 is " << count(v.begin(), v.end(), [](int i) {return i > 5;}) << endl;
+    cout << "number of values in v above 5 is " << count_if(v.begin(), v.end(), [](int i) {return i >= 5;}) << endl;
     printVector(v);
     printReverse(v);
     bigToSmall(v);
+    supprConsecutiveDoublons(v);
 
     vector<int>::iterator p = find_if(v.begin(), v.end(), [](int i) {
         return i > 4;
